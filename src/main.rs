e16233e8f04f4c0d62b038b462e65d8382bf78c5
Alexander <akrimec@gmail.com>
2024-11-19 23:27:41 +0200

    catalog update

--- src/main.rs
+++ src/main.rs
@@ -55,7 +55,28 @@ impl Catalog {
     fn add_item(&mut self, media: Media) {
         self.items.push(media);
     }
+
+    // fn get_by_index(&self, index: usize) -> MightHaveAValue {
+    //     if self.items.len() > index {
+    //         MightHaveAValue::ThereIsAValue(&self.items[index])
+    //     } else {
+    //         MightHaveAValue::NoValue
+    //     }
+    // }
+
+    fn get_by_index(&self, index: usize) -> Option<&Media> {
+        if self.items.len() > index {
+            Some(&self.items[index])
+        } else {
+            None
+        }
+    }
 }
+
+// enum MightHaveAValue<'a> {
+//     ThereIsAValue(&'a Media),
+//     NoValue
+// }
 
 // fn print_book(book: &Media) {}
 // fn print_movie(book: &Media) {}
@@ -94,6 +115,83 @@ fn main() {
     catalog.add_item(podcast);
     catalog.add_item(placeholder);
 
-    println!("{:#?}", catalog)
+    // println!("{:#?}", catalog.items.get(0));
+    //
+    // match catalog.items.get(100) {
+    //     Some(value) => println!("{:#?}", value),
+    //     None => println!("Nothing found")
+    // }
+
+    // let items = catalog.get_by_index(4);
+    //
+    // println!("{:#?}", item);
+
+    // match catalog.get_by_index(0) {
+    //     MightHaveAValue::ThereIsAValue(value) => println!("{:#?}", value),
+    //     MightHaveAValue::NoValue => println!("No value"),
+    // }
+
+    // if let MightHaveAValue::ThereIsAValue(value) = catalog.get_by_index(1) {
+    //     println!("There is a value {:#?}", value);
+    // } else {
+    //     println!("There is not value");
+    // }
+    if let Some(value) = catalog.get_by_index(1) {
+        println!("There is a value {:#?}", value);
+    } else {
+        println!("There is not value");
+    }
+
+    let item = catalog.get_by_index(40);
+    let placeholder = Media::Placeholder;
+    // print!("{:#?}", item.unwrap());
+    //
+    // print!("{:#?}", item.expect("Must be some value"));
+
+    print!("{:#?}", item.unwrap_or(&placeholder));
 
 }
+
+
+// #[derive(Debug)]
+// struct Account {
+//     balance: i32
+// }
+//
+// fn main() {
+//     let mut accounts: Vec<Account> = vec![
+//         Account { balance: 0 },
+//         Account { balance: 10 }
+//     ];
+//
+//     // Add code here:
+//     match accounts.get_mut(10) {
+//         Some(value) => {
+//             value.balance = 30;
+//             println!("{:#?}", value);
+//         }
+//         None => println!("No account found"),
+//     }
+// }
+
+
+// #[derive(Debug)]
+// struct Account {
+//     balance: i32
+// }
+//
+// fn main() {
+//     let mut accounts: Vec<Account> = vec![
+//         Account { balance: 0 },
+//         Account { balance: 10 }
+//     ];
+//
+//     // Add code here:
+//     match accounts.first_mut() {
+//         Some(value) => {
+//             value.balance = 30;
+//             println!("{:#?}", value);
+//         }
+//         None => println!("No account found"),
+//     }
+// }
